8492dace4cc23b0098cff43c27198a09ef921e1e
Jenna Hartley <jenna.hartley@pushinteractions.com>
2019-01-08 09:12:56 -0600

    Added base html and css

--- login/views.py
+++ login/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+
+            login(request, user)
+            return redirect('home')  # todo
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
