0f41751cbb8ced770172682fe65d21bd78de2a65
Jenna Hartley <jenna.hartley@live.ca>
2019-05-19 10:46:15 -0600

    More changes to staticfiles_Dir

--- bingo/settings.py
+++ bingo/settings.py
@@ -136,5 +136,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bingo/static/'),
-                    os.path.join(BASE_DIR, 'games/static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bingo', 'static'),
+                    os.path.join(BASE_DIR, 'games', 'static')]
